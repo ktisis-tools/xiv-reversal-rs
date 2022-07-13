c36c2711d4e1f205b716a59ed10c0d6f76a00ccd
chirp <ch1rp@protonmail.com>
2022-07-13 14:31:54 +0100

    Implement Process struct

---
+++ src/process/process.rs
@@ -0,0 +1,84 @@
+// Dependencies
+
+use std::{
+	mem,
+	ptr::null,
+	os::windows::ffi::OsStringExt,
+	collections::HashMap,
+	ffi::{
+		OsString,
+		c_void
+	}
+};
+
+use winapi::um::{
+	libloaderapi::GetModuleHandleW,
+	processthreadsapi::{
+		GetProcessId,
+		GetCurrentProcess
+	},
+	tlhelp32::{
+		CreateToolhelp32Snapshot as CreateTH32,
+		Module32NextW,
+		MODULEENTRY32W,
+		TH32CS_SNAPMODULE
+	},
+	handleapi::INVALID_HANDLE_VALUE,
+	winnt::HANDLE
+};
+
+use process::Module;
+use memory::MemRegion;
+
+// Process
+
+pub struct Process {
+	pub pid: u32,
+	pub handle: HANDLE,
+	pub base: *mut c_void,
+	pub modules: HashMap<String, Module>,
+	pub memory: MemRegion
+}
+
+impl Process {
+	pub fn get() -> Self {
+		let handle = unsafe { GetCurrentProcess() };
+		let pid = unsafe { GetProcessId(handle) };
+		let base = unsafe { GetModuleHandleW(null()) } as _;
+		let modules = Self::get_modules(pid);
+		
+		let proc_mod = modules.values().find(|mod| mod.memory.base == base).unwrap();
+		let memory = MemRegion::new(base, proc_mod.memory.size);
+
+		Self {
+			handle,
+			pid,
+			base,
+			modules,
+			memory
+		}
+	}
+
+	pub fn get_modules(pid: u32) -> HashMap<String, Module> {
+		let mut modules = HashMap::<String, Module>::new();
+
+		let handle = unsafe { CreateTH32(TH32CS_SNAPMODULE, pid) };
+		assert!(handle != INVALID_HANDLE_VALUE, "TH32 returned invalid handle.");
+
+		let mut entry: MODULEENTRY32W = unsafe { mem::zeroed() };
+		entry.dwSize = mem::size_of::<MODULEENTRY32W>() as _;
+
+		while unsafe { Module32NextW(handle, &mut entry) } != {
+			if let Ok(name) = OsString::from_wide(&entry.szModule).into_string() {
+				let module = Module::new(
+					entry.hModule,
+					entry.modBaseAddr as _,
+					entry.modBaseSize as _
+				);
+				modules.insert(name.trim_matches('\0').trim().to_string(), module);
+			}
+		}
+
+		return module;
+	}
+}
