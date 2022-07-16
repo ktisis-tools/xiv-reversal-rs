041514e1ce18a773b2166f1a7ab9bf783ee23fa0
chirp <ch1rp@protonmail.com>
2022-07-16 12:50:33 +0100

    Implement SigScanner

--- src/memory/scanner.rs
+++ src/memory/scanner.rs
@@ -1,1 +1,53 @@
-pub struct SigScanner {}
+// Dependencies
+
+use libc::c_void;
+use std::str::from_utf8;
+
+use memory::MemRegion;
+
+// SigScanner
+
+pub struct SigScanner {
+	region: MemRegion
+}
+
+impl SigScanner {
+	pub fn new(region: MemRegion) -> Self {
+		Self { region }
+	}
+
+	pub unsafe fn scan_bytes(&self, bytes: &[Option<u8>]) -> Option<*mut c_void> {
+		let len = bytes.len();
+		
+		'a: for i in 0 .. (self.region.size as usize - len) {
+			for (x, byte) in bytes.iter().enumerate() {
+				let byte = match byte {
+					Some(s) => *s,
+					None => continue
+				};
+
+				if *(self.region.base.add(i + x) as *const u8) != byte {
+                    continue 'a;
+                }
+			}
+
+			return Some(self.region.base.add(i) as _);
+		}
+
+		return None;
+	}
+
+	pub fn scan(&self, text: &str) -> Option<*mut c_void> {
+		let bytes_str: Vec<&str> = text.split(" ").collect();
+
+		let bytes: Vec<Option<u8>> = bytes_str.into_iter().map(|x| {
+			if x == "??" {
+				None
+			} else {
+				Some(u8::from_str_radix(x, 16).unwrap())
+			}
+		}).collect();
+		
+		unsafe { self.scan_bytes(&bytes) }
+	}
+}
